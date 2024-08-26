e7b61bf850f8f229118ad90e1151e8f7d36545d5
tarunja1ks <tarunjaiks@gmail.com>
2024-08-25 23:52:12 -0700

    Removing the try catch

--- Verification.sh
+++ Verification.sh
@@ -1,108 +1,101 @@
 #!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status
-trap 'catch' ERR
-
-try() {
-
-    brew --version
-
-    ruby -v
-    # Bundler version, it is part of Ruby install
-    bundle -v
-
-    # Show active Python version, it needs to be 3.10 or better
-    python --version
-
-    # Show Jupyter packages, nbconvert needs to be in the list of installed
-    jupyter --version
-    jupyter kernelspec list  
-
-    git config --global --list
 
 
-    source /tmp/variables.sh
+Passed_Checks=false
 
-    echo "Project dir: $project_dir"
-    echo "Project: $project"
-    echo "Repo: $project_repo"
+brew --version
+
+ruby -v
+# Bundler version, it is part of Ruby install
+bundle -v
+
+# Show active Python version, it needs to be 3.10 or better
+python --version
+
+# Show Jupyter packages, nbconvert needs to be in the list of installed
+jupyter --version
+jupyter kernelspec list  
+
+git config --global --list
 
 
+source /tmp/variables.sh
 
-    source /tmp/variables.sh
-
-    echo "Using conditional statement to create a project directory and project"
-
-    cd ~    # start in home directory
-
-    # Conditional block to make a project directory
-    if [ ! -d $project_dir ]
-    then 
-        echo "Directory $project_dir does not exist... making directory $project_dir"
-        mkdir -p $project_dir
-    fi
-    echo "Directory $project_dir exists." 
-
-    # Conditional block to git clone a project from project_repo
-    if [ ! -d $project ]
-    then
-        echo "Directory $project does not exist... cloning $project_repo"
-        cd $project_dir
-        git clone $project_repo
-        cd ~
-    fi
-    echo "Directory $project exists."
+echo "Project dir: $project_dir"
+echo "Project: $project"
+echo "Repo: $project_repo"
 
 
 
+source /tmp/variables.sh
 
-    # Listing out the files in the repo
+echo "Using conditional statement to create a project directory and project"
 
-    echo "Navigate to project, then navigate to area wwhere files were cloned"
-    cd $project
-    pwd
+cd ~    # start in home directory
 
-    echo ""
-    echo "list top level or root of files with project pulled from github"
-    ls
+# Conditional block to make a project directory
+if [ ! -d $project_dir ]
+then 
+    echo "Directory $project_dir does not exist... making directory $project_dir"
+    mkdir -p $project_dir
+fi
+echo "Directory $project_dir exists." 
 
-
-
-    #Showing read.md stuff
-    echo "Navigate to project, then navigate to area wwhere files were cloned"
-
-    cd $project
-    echo "show the contents of README.md"
-    echo ""
-
-    cat README.md  # show contents of file, in this case markdown
-    echo ""
-    echo "end of README.md"
-
-
-    #Show env variables
-    echo "Show the shell environment variables, key on left of equal value on right"
-    echo ""
-
-    env
+# Conditional block to git clone a project from project_repo
+if [ ! -d $project ]
+then
+    echo "Directory $project does not exist... cloning $project_repo"
+    cd $project_dir
+    git clone $project_repo
+    cd ~
+fi
+echo "Directory $project exists."
 
 
 
 
+# Listing out the files in the repo
 
-    cd $project
+echo "Navigate to project, then navigate to area wwhere files were cloned"
+cd $project
+pwd
 
-    echo ""
-    echo "show the secrets of .git config file"
-    cd .git
-    ls -l config
-
-    echo ""
-    echo "look at config file"
-    cat config
-}
+echo ""
+echo "list top level or root of files with project pulled from github"
+ls
 
 
-catch() {
-    echo "verifications not passed"
-}
-try
+
+#Showing read.md stuff
+echo "Navigate to project, then navigate to area wwhere files were cloned"
+
+cd $project
+echo "show the contents of README.md"
+echo ""
+
+cat README.md  # show contents of file, in this case markdown
+echo ""
+echo "end of README.md"
+
+
+#Show env variables
+echo "Show the shell environment variables, key on left of equal value on right"
+echo ""
+
+env
+
+
+
+
+
+cd $project
+
+echo ""
+echo "show the secrets of .git config file"
+cd .git
+ls -l config
+ierwhlfiwelfj
+echo ""
+echo "look at config file"
+cat config
+
